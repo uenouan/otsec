f1c462307addfa8a82f256bbd2ef40dc0974280c
uenotakabon <uenotakabon@gmail.com>
2025-01-03 19:41:02 +0900

    add file

---
+++ ptest.py
@@ -0,0 +1,24 @@
+from pymodbus3.client.sync import ModbusTpClient
+
+client10 = ModbusTpClient('192.168.95.10')
+client11 = ModbusTpClient('192.168.95.11')
+client12 = ModbusTpClient('192.168.95.12')
+client13 = ModbusTpClient('192.168.95.13')
+
+try:
+
+    while True:
+        res = client10.write_register(1, 65535)
+        print("10:", res)
+        res = client11.write_register(1, 65535)
+        print("11:", res)
+        res = client12.write_register(1, 65535)
+        print("12:", res)
+        res = client13.write_register(1, 65535)
+        print("13:", res)
+
+except KeyboardInterrupt:
+    client10.close()
+    client11.close()
+    client12.close()
+    client13.close()
