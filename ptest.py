64040a8d237e0e9d8ab5fbd3dc5621ecc8b6f4be
uenouan <uenotakabon@gmail.com>
2025-04-06 15:31:42 +0900

    updated

--- ptest.py
+++ ptest.py
@@ -7,10 +7,10 @@ valve2 = ModbusTcpClient('192.168.95.12')
 
 try:
     match args[1]:
-        case fullopen:
+        case 'open':
             while True:
                 valve1.write_register(1, 65535)
-        case shutoff:
+        case 'close':
             while True:
                 valve2.write_register(1, 0)
 except KeyboardInterrupt:
